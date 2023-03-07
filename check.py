f931654721c0f6946740c5388448d58dd8258260
Sparsha Bindu B S <sparshabsbindu@gmail.com>
2023-03-07 20:12:01 +0530

    changed check.py

--- check.py
+++ check.py
@@ -1,17 +1,8 @@
 def find_max(numbers):
-    """
-    Finds the maximum number in a list of integers.
-
-    Args:
-        numbers: a list of integers
-
-    Returns:
-        The maximum number in the list.
-    """
     if not numbers:
         return None
     max_num = numbers[0]
     for num in numbers:
         if num > max_num:
             max_num = num
-    return max_num
+    return max_num
